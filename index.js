523ecb0c008924844b7fc3dfb1697a8990d8f522
andrea penso <andreap.dev@gmail.com>
2020-10-19 12:41:12 +0200

    2- arrow functions

--- index.js
+++ index.js
@@ -28,6 +28,67 @@ let li2 =
     </div>
 </li>`;
 
-console.log(li);
-console.log(li2);
+
+
+//Exercici 2
+//Converteix a funcions fletxa:
+
+//1
+function multiply(num1, num2) {
+   return num1 * num2;
+}
+
+let multiply2 = (num1, num2) =>  num1*num2;
+
+//2
+function toCelsius(fahrenheit) {
+   return (5/9) * (fahrenheit-32);
+}
+
+let toCelsius2 = fahrenheit => {
+    return (5/9)*(fahrenheit-32);
+};
+
+//3
+function padZeros(num, totalLen) {
+   var numStr = num.toString();
+   var numZeros = totalLen - numStr.length;
+   for (var i = 1; i <= numZeros; i++) {
+      numStr="0" + numStr; 
+      } 
+   return numStr;
+}
+
+let padZeros2 = (num, totalLen) => {
+    let numStr = num.toString();
+    let numZeros = totalLen - numStr.length;
+    for (let i = 1; i <= numZeros; i++) {
+       numStr="0" + numStr; 
+       } 
+    return numStr;
+};
+
+//4
+function power(base, exponent) { 
+   var result=1; 
+   for (var i=0; i < exponent; i++) { 
+         result *=base; 
+   } 
+   return result;
+}
+
+let power2 = (base, exponent) =>{
+    let result=1; 
+    for (let i=0; i < exponent; i++) { 
+          result *=base; 
+    } 
+    return result;
+};
+
+//5 
+function greet(who) {
+    console.log("Hello " + who);
+}
+
+let greet2 = who => console.log(`Hello ${who}`);
 
